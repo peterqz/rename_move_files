f56eef90728603134d21c6d70862ea07cb7c9b2b
peterqz <peterqz@users.noreply.github.com>
2020-10-12 20:16:07 -0500

    Update rename_move_files.py

--- rename_move_files.py
+++ rename_move_files.py
@@ -1,42 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[32]:
-
-
+#Enumerate and move many files 
+#by Peter Qz.
 import glob 
 import os
   
    
   
 # Asigna el directorio a la variable files. 
-print("Using glob.glob()") 
 files = glob.glob('/home/peter/Téléchargements/Nueva carpeta/*/*.jpg',  
                    recursive = True) 
 
-print(files) 
 # En el ciclo enumera los archivos, posterior los mueve  y cambia nombre al directorio.
 for i, files in  enumerate(files): 
     os.rename(files,
           "/home/peter/Téléchargements/test/" + str("Imagen00")+ str(i) + ".jpg" )
     print(i,files) 
     
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[3]:
-
-
-
-
